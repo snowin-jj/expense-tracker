3a5754773d3aaba1dcfc547a10b94c3113c4dfb7
devsnowin <snowin.developer@gmail.com>
2024-07-24 00:25:35 +0530

    connect frontend with backend

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,17 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRouter } from './routers/expenses'
+import { serveStatic } from 'hono/bun'
 const app = new Hono()
 
+// middlewares
 app.use('*', logger())
 
+// routes
 app.route('/api/expenses', expensesRouter)
+
+// static files
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
